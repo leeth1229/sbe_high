799448ebf25accf608312231e8cd396d7c741ee7
2_ttao <106638850+leeth1229@users.noreply.github.com>
2024-04-08 23:38:10 +0900

    Update function.py

--- function.py
+++ function.py
@@ -2,9 +2,28 @@ import streamlit as st
 import pandas as pd, numpy as np
 import streamlit_antd_components as sac
 
-@st.cache_data
+
+# 컴포넌트 생성을 위한 함수
+def create_section(header, subheaders, items, key_prefix):
+    st.subheader(header)
+    selected_items = []
+    for idx, (subheader, item_list) in enumerate(zip(subheaders, items)):
+        col1, col2 = st.columns([1,3])
+        with col1:
+            selected = sac.chip([
+                sac.ChipItem(label=subheader)
+            ], align='start', multiple=True, key=f"{key_prefix}_select_{idx}")  
+        with col2:
+            if len(selected) > 0:
+                selected_subitems = sac.chip([
+                    sac.ChipItem(label=item) for item in item_list
+                ], align='start', variant='outline', multiple=True, key=f"{key_prefix}_subselect_{idx}_{len(selected)}")
+                selected_items.extend(selected_subitems)
+    return selected_items
+
+
 def risk_function(area, eq):
-    df = pd.read_excel("/home/dataiku/workspace/code_studio-versioned/streamlit/docs/risk_factors/risk_factor.xlsx")
+    df = pd.read_excel("/Users/2ttao/Downloads/code/streamlit/docs/sbe/risk_factor.xlsx")
     risk_area = []
     for i in range(len(area)):
         risk_area.append(df[df["위험인자"] == area[i]]["위험변수"].tolist())
@@ -23,8 +42,3 @@ def risk_function(area, eq):
     etcs = list(set(all_risk_factors).difference(sames).difference(risk_area).difference(risk_eq))
 
     return {"sames": sames, "risk_area": risk_area, "risk_eq": risk_eq, "etcs": etcs}
-
-@st.cache_data
-def ChipItems(items):
-    chip_items = [sac.ChipItem(label=item) for item in items]
-    return chip_items
